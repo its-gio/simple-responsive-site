a5b6a3e659639649d0f89ea5b02688b186b1fe52
Giovani Aguirre <me@its-g.io>
2021-06-09 17:13:26 -0700

    Auto playing video on click of learn more button

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,10 @@
+// https://www.youtube.com/embed/NBppslasYOM?autoplay=1&mute=1
 let modal = document.querySelector("#modal");
+const iframe = modal.querySelector("iframe");
 
 function toggleModal() {
   modal.classList.toggle("active");
+  modal.classList.contains("active") ?
+    iframe.src = "https://www.youtube.com/embed/NBppslasYOM?autoplay=1&mute=1" :
+    iframe.src = "#";
 }
